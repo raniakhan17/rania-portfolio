215e9a20ba075ebc4d69574ca8bc736d148f156f
Rania Khan <raniakhan@Ranias-MacBook-Pro.local>
2026-08-04 18:03:15 -0500

    Remove portfolio loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import { Hero } from '@/components/Hero';
 import { PortfolioTabs } from '@/components/PortfolioTabs';
-import PortfolioLoader from '@/components/PortfolioLoader';
 
 export default function Home() {
   return (
-    <>
-      <PortfolioLoader />
-      <main className="page">
-        <Hero />
-        <PortfolioTabs />
-      </main>
-    </>
+    <main className="page">
+      <Hero />
+      <PortfolioTabs />
+    </main>
   );
 }
